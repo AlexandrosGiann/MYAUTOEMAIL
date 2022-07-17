3ed6a78240b72fc77b499ff8fe86c3247845311b
AlexandrosGiann <69394899+AlexandrosGiann@users.noreply.github.com>
2022-07-18 00:47:32 +0300

    Update send_emails.py

--- send_emails.py
+++ send_emails.py
@@ -9,3 +9,8 @@ text = """
 def send_email(username, password, emails, subject, text):
     yag = yagmail.SMTP(username, password)
     yag.send(emails,subject,text)
+
+
+if __name__ == '__main__':
+    emails = [email for email in open('saved_emails.txt', 'r').readlines()]
+    send_email(USERNAME, PASSWORD, emails, subject, text)
