b4f4390178c03407482573499f933ebf8d791687
AlexandrosGiann <69394899+AlexandrosGiann@users.noreply.github.com>
2022-07-18 00:39:22 +0300

    Update send_emails.py

--- send_emails.py
+++ send_emails.py
@@ -1,4 +1,11 @@
 import yagmail
+
+USERNAME = ""
+PASSWORD = ""
+subject = ""
+text = """
+"""
+
 def send_email(username, password, emails, subject, text):
     yag = yagmail.SMTP(username, password)
     yag.send(emails,subject,text)
